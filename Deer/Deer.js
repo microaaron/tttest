586e2dc6217c84602b1f854445eb6e17668475b4
microaaron <microaaron@live.cn>
2020-06-26 07:24:08 +0800

    Add files via upload

---
+++ Deer/Deer.js
@@ -0,0 +1,34 @@
+var Deer = {
+	nameSpaceName:"Deer",
+	mapDEFToID:"true",
+	//onclick:'redNose()',
+	url:"./Deer/Deer.x3d",
+	
+	redNose:function(event){
+		//console.log(event.target);
+		//console.log(event.currentTarget );
+		//console.log(this );
+		var id =$(event.currentTarget).attr("id");
+		//debugger
+        if(document.getElementById(id+'__MA_Nose').getAttribute('diffuseColor')!= '1 0 0')
+            document.getElementById(id+'__MA_Nose').setAttribute('diffuseColor', '1 0 0');
+        else
+            document.getElementById(id+'__MA_Nose').setAttribute('diffuseColor', '0 0 0');
+			//document.getElementById(id+'__Deer_Armature_TRANSFORM').setAttribute('translation', '0 0 0');
+			console.log($(event.currentTarget) );
+			console.log($(event.target));
+			//$(event.currentTarget).hide();
+			$(event.currentTarget).attr("render","false");
+    },
+	init:function(){
+		
+		//debugger
+		return;
+	}
+}
+
+/*function tt(){
+	alert("this is deer.js");
+}
+*/
+
